1a4d9affd4c6be256db2f309631faf430503e195
kedrov <qaschoolusa@gmail.com>
2025-06-21 03:37:09 -0400

    Fixed the page insight issues.

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,7 +1,7 @@
-import ContactForm from "@/components/ContactForm";
 import AboutSection from "@/components/HomePage/AboutSection";
 import HeroSection from "@/components/HomePage/HeroSection";
 import ServicesSection from "@/components/HomePage/ServicesSection";
+import ContactForm from "@/components/ContactForm";
 
 export default function Home() {
   return (
